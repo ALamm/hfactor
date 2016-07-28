e0bb3c54448787373cc1ca61fdc7e4d9513c1a5c
Alexander Lamm <mr.alexanderlamm@gmail.com>
2016-07-27 17:28:29 -0700

    version 1.0

--- server/config/_config.js
+++ server/config/_config.js
@@ -6,27 +6,27 @@ var ids = {
   github: {
     clientID: process.env.GITHUBCLIENT,
     clientSecret: process.env.GITHUBSECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    callbackURL: process.env.GITHUBCALLBACK
   },
   linkedin: {
     clientID: process.env.LINKEDINCLIENT,
     clientSecret: process.env.LINKEDINSECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+    callbackURL: process.env.LINKEDINCALLBACK
   },
   twitter: {
     consumerKey: process.env.TWITTERKEY,
     consumerSecret: process.env.TWITTERSECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+    callbackURL: process.env.TWITTERCALLBACK
   },
     facebook: {
     clientID: process.env.FACEBOOKCLIENT,
     clientSecret: process.env.FACEBOOKSECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/facebook/callback"
+    callbackURL: process.env.FACEBOOKCALLBACK
   },
     google: {
     clientID: process.env.GOOGLECLIENT,
     clientSecret: process.env.GOOGLESECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+    callbackURL: process.env.GOOGLECALLBACK
   }
 };
 
